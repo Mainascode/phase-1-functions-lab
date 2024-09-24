489e1eed84af526aace157c5b2975c92bcf203cf
Maina <smurkcarter855@gmail.com>
2024-09-24 11:59:44 +0300

    origin

--- index.js
+++ index.js
@@ -1,1 +1,24 @@
 // Code your solution in this file!
+function distanceFromHqInBlocks(distance){
+   return Math.abs(42-distance);
+}
+function distanceFromHqInFeet(somevalue){
+    return Math.abs(somevalue-42)*264;
+}
+function distanceTravelledInFeet(somevalue1, somevalue2){
+    return Math.abs(somevalue2-somevalue1)*264;
+}
+function calculatesFarePrice(somevalue1, somevalue2){
+    if(Math.abs ((somevalue2-somevalue1)*264)<400){
+        return 0;
+    }
+    else if((Math.abs (somevalue2-somevalue1)*264>400 && (Math.abs(somevalue2-somevalue1)*264)<2000)){
+        return(Math.abs (somevalue2-somevalue1)*264-400)*0.02;
+    }
+    else if ((Math.abs (somevalue2-somevalue1)*264)>2000 && (Math.abs (somevalue2-somevalue1)*264)<2500){
+        return 25
+    }
+    else if ((Math.abs (somevalue2-somevalue1)*264)>2500){
+        return 'cannot travel that far'
+    }
+}
